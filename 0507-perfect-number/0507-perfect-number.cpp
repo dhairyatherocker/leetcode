9fc6a6c85f2e3d63ffb1424e26f16ceb1f1a43a7
dhairyatherocker <dhairyarathore8957@gmail.com>
2026-04-23 17:55:10 +0530

    Time: 2136 ms (5.36%), Space: 7.7 MB (87.76%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+    int count=0;
+    for(int i=1;i<num;i++){
+    if(num%i==0) count+=i;    
+    }
+    if(count==num) return true;
+    return false;     
+    }
+};
