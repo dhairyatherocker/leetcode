dcabc463bf7cc3281e75739892e846746203fd44
dhairyatherocker <dhairyarathore8957@gmail.com>
2026-04-27 16:55:54 +0530

    Time: 19 ms (80.67%), Space: 18 MB (11.27%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-    // set<vector<int>>st;
-    vector<vector<int>>ans;
+    set<vector<int>>ans;
+    vector<vector<int>>res;
     if(nums.size()<4) return {};
     sort(nums.begin(),nums.end());
     for(int i=0;i<nums.size()-3;i++){
@@ -13,21 +13,18 @@ public:
     int right=nums.size()-1;
     while(left<right){
    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
-
-                    if(sum == target){
-                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
-
-                        while(left < right && nums[left] == nums[left+1]) left++;
-                        while(left < right && nums[right] == nums[right-1]) right--;
-
-                        left++;
-                        right--;
-                    }
-                    else if(sum < target) left++;
-                    else right--;   
+    if(sum == target){
+     ans.insert({nums[i], nums[j], nums[left], nums[right]});
+    // while(left < right && nums[left] == nums[left+1]) left++;
+    // while(left < right && nums[right] == nums[right-1]) right--;
+     left++;
+     right--;}
+    else if(sum < target) left++;
+     else right--;   
     }    
     }    
     }  
-    return ans;  
+    for(auto it : ans) res.push_back(it);
+    return res;
     }
 };
