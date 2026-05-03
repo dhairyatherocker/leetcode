114f45658c94202e4be8a86e06ae3f8343aa02df
dhairyatherocker <dhairyarathore8957@gmail.com>
2026-05-03 12:13:45 +0530

    Time: 39 ms (20.87%), Space: 27 MB (52.48%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numofsub(int index1,int index2,string &s,string &t,vector<vector<int>>&dp){
+    if(index2<0) return 1;
+    if(index1<0) return 0;
+    if(dp[index1][index2]!=-1) return dp[index1][index2];
+    int take;
+    if(s[index1]==t[index2]) take=numofsub(index1-1,index2-1,s,t,dp)+numofsub(index1-1,index2,s,t,dp);
+    else take=numofsub(index1-1,index2,s,t,dp);
+    return dp[index1][index2]=take;    
+    }
+    int numDistinct(string s, string t) {
+    int n=s.size();
+    int m=t.size();
+    vector<vector<int>>dp(n,vector<int>(m,-1));
+    return numofsub(n-1,m-1,s,t,dp);    
+    }
+};
