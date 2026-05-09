b77ccb528011d0c7eb3c628adf7958f954bc92e3
dhairyatherocker <dhairyarathore8957@gmail.com>
2026-05-09 11:20:11 +0530

    Time: 8 ms (34.27%), Space: 19.9 MB (39.58%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -7,7 +7,7 @@ public:
     for(int i=0;i<prerequisites.size();i++){
     int first=prerequisites[i][0];
     int second=prerequisites[i][1];
-    indegree[first]++;
+    indegree[second]++;
     adj[first].push_back(second);
     adj[second].push_back(first);   
     }
