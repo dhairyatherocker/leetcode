90cec42f6e873efcbb5a0325a5d787369fe3e553
dhairyatherocker <dhairyarathore8957@gmail.com>
2026-08-21 12:55:47 +0530

    Time: 3 ms (2.58%), Space: 11.9 MB (5.28%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-    int end=nums.size()-1;
+    // int end=nums.size()-1;
+    // int n=nums.size()-1;
+    // while(end>0 && nums[end-1]<=nums[end]) end--;
+    // if(end==0) return true;
+    // int start=0;
+    // if(nums[start]<nums[n]) return false;
+    // while(start<end && nums[start+1]>=nums[start]) start++;
+    // if(start+1==end) return true;
+    // return false; 
+
     int n=nums.size()-1;
+    int end=n;
     while(end>0 && nums[end-1]<=nums[end]) end--;
-    if(end==0) return true;
+    vector<int>dup=nums;
+    sort(dup.begin(),dup.end());
     int start=0;
-    if(nums[start]<nums[n]) return false;
-    while(start<end && nums[start+1]>=nums[start]) start++;
-    if(start+1==end) return true;
-    return false; 
+    reverse(nums.begin()+end,nums.end());
+    reverse(nums.begin(),nums.begin()+end);
+    reverse(nums.begin(),nums.end());
+    if(dup==nums) return true;
+    return false;
     }
 };
